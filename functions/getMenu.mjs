fc44c44c0ba1fe20dc204e44f46603bfdbc51049
Max Gonzaga <maxgonzaga@outlook.com>
2025-02-17 20:51:07 -0300

    update

--- functions/getMenu.mjs
+++ functions/getMenu.mjs
@@ -4,7 +4,10 @@ export default async (req, context) => {
   const menuStore = getStore("menu");
   try {
     const menu = await menuStore.get("menu");
-    const response = new Response(menu);
+    const response = new Response(menu,
+      {
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      });
     return response;
   } catch (error) {
     const response = new Response({ status: 500 });
