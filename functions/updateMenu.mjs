ca0751aa44eee3fbd13bed8422614b3cdfeb496e
Max Gonzaga <maxgonzaga@outlook.com>
2025-02-17 19:57:25 -0300

    update

--- functions/updateMenu.mjs
+++ functions/updateMenu.mjs
@@ -3,8 +3,8 @@ import { getStore } from "@netlify/blobs";
 export default async (req, context) => {
   const menuStore = getStore("menu");
   try {
-    menuStore.setJSON("menu", req.body);
-    const response = new Response(req.body, { status: 204 });
+    menuStore.setJSON("menu", req.json());
+    const response = new Response(req.json(), { status: 204 });
     return response;
   } catch (error) {
     const response = new Response(menuStore.get("menu"), { status: 500 });
