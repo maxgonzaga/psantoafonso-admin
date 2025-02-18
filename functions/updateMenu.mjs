17b941f623b017bd3762b165c7197c39ea740855
Max Gonzaga <maxgonzaga@outlook.com>
2025-02-17 21:40:20 -0300

    update

--- functions/updateMenu.mjs
+++ functions/updateMenu.mjs
@@ -8,12 +8,12 @@ export default async (req, context) => {
     const response = new Response(
       JSON.stringify(requestBody),
       {
-        headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': '*' },
+        headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': '*', 'Access-Control-Allow-Headers': '*' },
         status: 200
       });
     return response;
   } catch (error) {
-    const response = new Response(null, { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } });
+    const response = new Response(null, { status: 500, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': '*', 'Access-Control-Allow-Headers': '*' } });
     return response;
   }
 };
